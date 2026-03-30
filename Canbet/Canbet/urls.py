cc116b8ca7f47dbe25d2bf291f099b1f5d560dbf
zlyvers <zl943524@ohio.edu>
2026-03-30 14:12:13 -0400

    updated python files for updated JS

--- Canbet/Canbet/urls.py
+++ Canbet/Canbet/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('api/leaderboard/', views.api_leaderboard, name='api_leaderboard'),
     path('api/recent-opens/', views.api_recent_opens, name='api_recent_opens'),
     path('register/', views.register_view, name='register'),
+    path('api/canvas/sync/', views.api_canvas_sync, name='api_canvas_sync'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
